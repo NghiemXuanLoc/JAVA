8e801dc8b2a4fc6265096b34a0e1babc861874d8
NghiemXuanLoc <nghiemxuanloc02@gmail.com>
2023-02-28 00:07:29 +0700

    28/2/2023

--- JAVA_T2/src/Contest3/test.java
+++ JAVA_T2/src/Contest3/test.java
@@ -7,14 +7,19 @@ package Contest3;
 
 import java.util.Scanner;
 
-/**
- *
- * @author PC
- */
 public class test {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        int n = sc.nextInt();
+        int []a = new int[n];
+        for(int i = 0;i < n;i++){
+            a[i] = sc.nextInt();
+        }
+        for(int i = 0;i < n;i++){
+            System.out.println(a[i]);
+        }
     }
-}
+    
+    }
+
+
